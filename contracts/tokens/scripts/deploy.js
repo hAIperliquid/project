356184dcc9f9502ec13c2ec40c36fbf0b0885c48
Noobmaster169 <mariotaning@gmail.com>
2025-02-28 09:08:29 +0800

    Added New Token Smart Contract

--- contracts/tokens/scripts/deploy.js
+++ contracts/tokens/scripts/deploy.js
@@ -49,13 +49,13 @@ main()
 // Contract Verify:
 // npx hardhat verify --network polygonAmoy 0x5E559412a9Ea6552829ee8E833bF4947b2e1DdD0 0x74EF2a3c2CC1446643Ab59e5b65dd86665521F1c
 
-// npx hardhat verify --contract contracts/USDT.sol:USDT --network polygonAmoy 0x0504eF1192366Ac7Bf90c03964C53358C172A1A6 0x74EF2a3c2CC1446643Ab59e5b65dd86665521F1c
+// npx hardhat verify --contract contracts/USDC.sol:USDC --network polygonAmoy 0x69E91edE2DD58542f560ec976C160e37C8C256FB 0x74EF2a3c2CC1446643Ab59e5b65dd86665521F1c
 
 
 // HAI Token deployed: 0x5E559412a9Ea6552829ee8E833bF4947b2e1DdD0
 // Mock BTC Token deployed: 0x39683204f4822A75A3264a9e6583e9105fAD3fAc
 // Mock ETH Token deployed: 0x88ad8906909211929a71fE5A070e651b588D98e4
 // Mock USDT Token deployed: 0x0504eF1192366Ac7Bf90c03964C53358C172A1A6
-
+// Mock USDC Token deployed: 0x69E91edE2DD58542f560ec976C160e37C8C256FB
 
 // USA Token deployed: 0x5E559412a9Ea6552829ee8E833bF4947b2e1DdD0
